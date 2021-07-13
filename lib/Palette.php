7119a05b027bd6b24897fbec4312138989628841
Dustin Wilson <dustin@dustinwilson.com>
2021-07-13 13:52:32 -0500

    Added support for saving palettes in ACO format

--- lib/Palette.php
+++ lib/Palette.php
@@ -18,6 +18,62 @@ class Palette {
         return true;
     }
 
+    // Outputs Adobe Photoshop's ACO format
+    public function saveACO(): string {
+        if (count($this->colors) === 0) {
+            throw new \Exception("There must be a color in the palette to be able to save as a palette file format.\n");
+        }
+
+        $output = '';
+
+        // Do the version 1 swatches first for backwards compatibility reasons.
+        $count = count($this->colors, COUNT_RECURSIVE);
+        $output .= pack('nn', 1, $count);
+
+        $swatches = [];
+        foreach ($this->colors as $i => $c) {
+            // L*a*b* is used because RGB color values change when the color profile changes in
+            // Photoshop.
+            $l = $c->Lab->L * 100 / 255;
+            $a = $c->Lab->a * 100 / 255;
+            $b = $c->Lab->b * 100 / 255;
+
+            $swatches[$i] = pack('nCCccccn', 7, $l, $l, $a, $a, $b, $b, 0);
+            $output .= $swatches[$i];
+        }
+
+        // Then, the version 2 swatch data...
+        $output .= pack('nn', 2, $count);
+        foreach ($swatches as $i => $s) {
+            // Swatches in Version 2 are identical. The difference is the name which comes
+            // after.
+            $output .= $s;
+
+            $output .= pack('n', 0);
+            $name = mb_convert_encoding($this->colors[$i]->name, 'UTF-16');
+            $output .= pack('n', mb_strlen($name, 'UTF-16') + 1);
+            $output .= $name;
+
+            $output .= pack('n', 0);
+        }
+
+        return $output;
+    }
+
+    // Outputs to Adobe Photoshop's ACO format
+    public function saveACOFile(string $filename): bool {
+        $dirname = dirname($filename);
+        if (!is_dir($dirname)) {
+            throw new \Exception("Directory \"$dirname\" does not exist.\n");
+        }
+        if (!is_writable($dirname)) {
+            throw new \Exception("Directory \"$dirname\" is not writable.\n");
+        }
+
+        file_put_contents($filename, $this->saveACO());
+        return true;
+    }
+
     public function saveKPL(int $columnCount = 8, bool $readonly = false): string {
         $tmpfile = $this->createKPLTemp($columnCount, $readonly);
         $output = file_get_contents($tmpfile);
